0585534bfa4956afa4dcd03b646d0bbf0c530cac
mindfulme <go.go.gg.rostislav@gmail.com>
2021-03-19 19:24:02 +0200

    rm usd

--- usd/static/js/13.ead97cc8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpiearn-finance"]=this["webpackJsonpiearn-finance"]||[]).push([[13],{1568:function(n,i){}}]);
-//# sourceMappingURL=13.ead97cc8.chunk.js.map
--- usd/static/js/14.7ee5d4ad.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpiearn-finance"]=this["webpackJsonpiearn-finance"]||[]).push([[14],{1655:function(n,i){}}]);
-//# sourceMappingURL=14.7ee5d4ad.chunk.js.map
